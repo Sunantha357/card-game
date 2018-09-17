a9b4472dbb63d8c1df1003456842f990448793bd
Sunantha <sunanta533848@hotmail.com>
2018-09-17 11:00:08 +0700

    Hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+const word = "Hello";
 class App extends Component {
  render() {
  return (
- <div className="App">
- Hello World
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
  </div>
  );
  }
 }
-export default App;
