cf07962086ff9605d5e581f557f603f04b203a3a
Sunantha <sunanta533848@hotmail.com>
2018-09-17 11:30:26 +0700

    Game Logic

--- src/WordCard.js
+++ src/WordCard.js
@@ -12,3 +12,26 @@ activationHandler = c => { console.log(`${c} has been activated.`) }
 ...
 <CharacterCard value={c} key={i} 
 activationHandler={this.activationHandler}/>
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+       word,
+       chars,
+       attempt: 1,
+       guess: [],
+       completed: false
+    }
+}
+activationHandler = (c) => {
+    let guess = [...this.state.guess, c]
+    this.setState({guess})
+    if(guess.length == this.state.chars.length){
+       if(guess.join('').toString() == this.state.word){
+          this.setState({guess: [], completed: true})
+       }else{
+          this.setState({guess: [], attempt: this.state.attempt + 1})
+       }
+    }
+   }
